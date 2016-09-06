5bfa91ef7dfad2747da223ac257d8c46aebe8b47
adrianmq <mateiadrian25@gmail.com>
2016-09-06 16:13:24 +0000

    fix info-icon display by removing redundant class with height/weight

--- app/views/loginView.php
+++ app/views/loginView.php
@@ -1,17 +1,15 @@
 <form method="POST" action="login" data-id="admin-login-form">
   <input type="email" data-id="admin-email" name="email" placeholder="Enter email address"/>
-    <div class="after-input tooltip">
+    <div class="after-input">
       <div id="tooltip" class="visible-icon"></div>
       <div id="tooltip-info" class="info"></div>
     </div>
     <li id="email-error" class="input-error hide"></li>
   <input type="password" data-id="admin-pass" name="password" placeholder="Enter password"/>
-    <div class="after-input tooltip">
+    <div class="after-input">
       <div id="tooltip" class="visible-icon"></div>
       <div id="tooltip-info" class="info"></div>
     </div>
     <li id="pass-error" class="input-error hide"></li>
-  <!--<input type="email" data-id="admin-email" name="email" placeholder="Enter your email"/>-->
-  <!--<input type="password" data-id="admin-pass" name="password" placeholder="Enter your password"/>-->
   <button id="admin-login">Login</button>
 </form>
